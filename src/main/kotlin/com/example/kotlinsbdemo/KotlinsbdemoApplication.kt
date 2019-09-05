318d67dcca17fbdfd46e85f213bb997c3bc8b5b8
MUKTEVI GOPI KRISHNA <muktevigk@handbookinc.com>
2019-09-05 19:46:02 +0530

    Working Repo

---
+++ src/main/kotlin/com/example/kotlinsbdemo/KotlinsbdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinsbdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinsbdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinsbdemoApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/kotlinsbdemo/beans/Greeting.kt
@@ -0,0 +1,3 @@
+package com.example.kotlinsbdemo.beans
+
+data class Greeting(val id: Long, val content: String)
